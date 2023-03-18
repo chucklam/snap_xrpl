90fc9160cf7cb0dcbb714f09d6ad3b3e23ddc10f
Chuck Lam <939400+chucklam@users.noreply.github.com>
2023-03-18 02:03:13 -0700

    Get Dogecoin account Followed https://docs.metamask.io/guide/snaps-concepts.html

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,9 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
+import {
+  BIP44CoinTypeNode,
+  getBIP44AddressKeyDeriver,
+} from '@metamask/key-tree';
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -11,9 +15,31 @@ import { panel, text } from '@metamask/snaps-ui';
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
+  // Get the Dogecoin node, corresponding to the path m/44'/3'
+  const dogecoinNode = await snap.request({
+    method: 'snap_getBip44Entropy',
+    params: {
+      coinType: 3,
+    },
+  });
+
+  /**
+   * Creates a function that takes an index and returns an extended private key for m/44'/3'/0'/0/address_index
+   * The second parameter to getBIP44AddressKeyDeriver is not passed. This sets account and change to 0
+   */
+  const deriveDogecoinAddress = await getBIP44AddressKeyDeriver(
+    dogecoinNode as BIP44CoinTypeNode, // TODO Check the type rather assume casting would work
+  );
+  const dogeAccount = await deriveDogecoinAddress(0);
+
   switch (request.method) {
     case 'hello':
+      console.log(`Dogecoin address: ${dogeAccount.address}`);
+
       return snap.request({
         method: 'snap_dialog',
         params: {
