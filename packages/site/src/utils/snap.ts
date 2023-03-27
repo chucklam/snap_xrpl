1a8d07b1ee0ced5c9d8380a7c4088f65e9926561
Chuck Lam <939400+chucklam@users.noreply.github.com>
2023-03-27 22:09:46 +0800

    Clean code + update README

--- packages/site/src/utils/snap.ts
+++ packages/site/src/utils/snap.ts
@@ -50,14 +50,13 @@ export const getSnap = async (version?: string): Promise<Snap | undefined> => {
   }
 };
 
-/**
- * Invoke the "hello" method from the example snap.
- */
-
 export const sendGetAddress = async () => {
   return window.ethereum.request({
     method: 'wallet_invokeSnap',
-    params: { snapId: defaultSnapOrigin, request: { method: 'get_address' } },
+    params: {
+      snapId: defaultSnapOrigin,
+      request: { method: 'get_address' },
+    },
   });
 };
 
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -10,7 +10,7 @@ import { Wallet } from 'xrpl';
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
  *
  * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
+ * args.origin - The origin of the request, e.g., the website that
  * invoked the snap.
  * @param args.request - A validated JSON-RPC request object.
  * @returns The result of `snap_dialog`.
@@ -46,21 +46,6 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
       console.log(`Derived account address: ${derivedAccount.address}`);
 
       return xrpAccount?.address;
-    /*
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'Confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-      */
     default:
       throw new Error('Method not found.');
   }
